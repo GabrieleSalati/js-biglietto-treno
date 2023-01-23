00d331db6935e4379e3420354905cbcc6bb57cde
GabrieleSalati <gabriele.salati5@gmail.com>
2023-01-23 17:30:03 +0100

    Added proper conditions for price calc

--- js/script.js
+++ js/script.js
@@ -4,6 +4,19 @@ console.log(tripLenght);
 
 const customerAge = prompt("Quanti anni hai?")
 
-let price = tripLenght * 0.21
+console.log(customerAge)
+
+if (customerAge < 18) {
+    price = tripLenght * 0.21 - 20 / 100;
+}
+
+else if (customerAge > 65) {
+    price = tripLenght * 0.21 - 40 / 100;
+}
+
+else {
+    price = tripLenght * 0.21;
+}
 
 console.log(price);
+
