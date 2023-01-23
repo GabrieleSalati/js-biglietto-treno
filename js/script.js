a6698f8b2fc4b21b708c30a2ed2ee661e790dbbe
GabrieleSalati <gabriele.salati5@gmail.com>
2023-01-23 19:51:13 +0100

    Corretta formula di calcolo

--- js/script.js
+++ js/script.js
@@ -7,11 +7,11 @@ const customerAge = prompt("Quanti anni hai?")
 console.log(customerAge)
 
 if (customerAge < 18) {
-    price = tripLenght * 0.21 - 20 / 100;
+    price = tripLenght * 0.21 * (1 - 20 / 100);
 }
 
 else if (customerAge > 65) {
-    price = tripLenght * 0.21 - 40 / 100;
+    price = tripLenght * 0.21 * (1 - 40 / 100);
 }
 
 else {
