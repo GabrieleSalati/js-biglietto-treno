48ff5e2521916c27bb0081bfec8ccf494e119f31
GabrieleSalati <gabriele.salati5@gmail.com>
2023-01-23 17:11:34 +0100

    Price distance calc function added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 const tripLenght = prompt("Quanti km e' lungo il tuo viaggio?")
 
+console.log(tripLenght);
+
 const customerAge = prompt("Quanti anni hai?")
+
+let price = tripLenght * 0.21
+
+console.log(price);
